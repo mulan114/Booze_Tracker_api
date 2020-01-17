b4851cc920fac0268a5389dd80c1e7cbaac9c5d5
mulan <mulan114@yahoo.com>
2020-01-17 11:53:33 -0500

    added mongo atlas

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 'use strict';
 
+// exports.DATABASE_URL =
+//   process.env.DATABASE_URL || "mongodb://localhost/booze_tracker_db";
+
+// exports.TEST_DATABASE_URL =
+//   process.env.TEST_DATABASE_URL || "mongodb://localhost/test_booze_tracker_db";
+
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/booze_tracker_db";
+  process.env.DATABASE_URL || "mongodb+srv://capstone3:capstone3@cluster0-zwatf.mongodb.net/test?retryWrites=true&w=majority";
 
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/test_booze_tracker_db";
+
 
 exports.PORT = process.env.PORT || 8080;
 
